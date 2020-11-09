6da7b344d8c8d1d6a730006c89b9624d639e8604
igor gorchakov <gorchakov.igiv@gmail.com>
2020-11-09 13:02:08 +0300

    changed on simple version of StackWithMax

--- src/main/java/org/sedgewick/algorithms/part_one/week_two/question_two/StackWithMax.java
+++ src/main/java/org/sedgewick/algorithms/part_one/week_two/question_two/StackWithMax.java
@@ -2,77 +2,51 @@ package org.sedgewick.algorithms.part_one.week_two.question_two;
 
 public class StackWithMax {
     private Node top;
-    private Node topMax;
 
     private class Node {
         private Node next;
-        private Node prevMax;
-        private Node nextMax;
         private int value;
+        private int max;
 
-        public Node(Node next, Node prevMax, Node nextMax, int value) {
+        public Node(Node next, int value, int max) {
             this.next = next;
-            this.prevMax = prevMax;
-            this.nextMax = nextMax;
             this.value = value;
+            this.max = max;
         }
     }
 
     public void push(int value) {
-        Node nextMax = topMax;
-        Node prevMax = null;
-        while (nextMax != null && nextMax.value >= value) {
-            prevMax = nextMax;
-            nextMax = nextMax.nextMax;
-        }
-
-        Node node;
-        if (prevMax == null) {
-            node = new Node(top, null, nextMax, value);
-            top = node;
-            topMax = node;
+        if (top == null){
+            top = new Node(null, value, value);
         } else {
-            node = new Node(top, prevMax, nextMax, value);
-            top = node;
-            prevMax.next = node;
+            Node prevTop = top;
+            top = new Node(prevTop, value, Math.max(prevTop.max, value));
         }
     }
 
     public int pop() {
         if (top == null) {
-            return Integer.MIN_VALUE;
+            return 0;
         }
 
         Node prevTop = top;
         top = prevTop.next;
-
-        Node prevMax = prevTop.prevMax;
-        Node nextMax = prevTop.nextMax;
-        if (prevMax == null){
-            topMax = nextMax;
-        } else {
-            prevMax.nextMax = prevTop.nextMax;
-            if (nextMax!= null){
-                nextMax.prevMax = prevTop.prevMax;
-            }
-        }
-
         return prevTop.value;
     }
 
     public int top() {
         if (top == null) {
-            return Integer.MIN_VALUE;
+            return 0;
         } else {
             return top.value;
         }
     }
 
     public int getMax() {
-        if (topMax == null) {
-            return Integer.MIN_VALUE;
+        if (top == null) {
+            return 0;
         } else {
-            return topMax.value;
+            return top.max;
         }
     }
 }
