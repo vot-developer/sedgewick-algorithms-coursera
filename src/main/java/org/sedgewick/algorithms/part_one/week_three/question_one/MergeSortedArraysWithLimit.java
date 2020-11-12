ac04acddc06bb09075451c86ce3db1a3a6c65e2d
igor gorchakov <gorchakov.igiv@gmail.com>
2020-11-12 20:54:43 +0300

    found better version of merge sorted arrays with limit

--- src/main/java/org/sedgewick/algorithms/part_one/week_three/question_one/MergeSortedArraysWithLimit.java
+++ src/main/java/org/sedgewick/algorithms/part_one/week_three/question_one/MergeSortedArraysWithLimit.java
@@ -2,42 +2,24 @@ package org.sedgewick.algorithms.part_one.week_three.question_one;
 
 public class MergeSortedArraysWithLimit {
 
-    public int[] merge(int[] nums1, int[] nums2, int n) {
-        if (n == 0)
+    public int[] merge(int[] a) {
+        if (a.length == 0)
             return new int[]{};
 
-        //regular merging
-        int[] auxiliary = new int[n];
-        int i = 0, j = 0;
-        for (int k = 0; k < n; k++){
-            if (j < n && (i == n || nums2[j] <= nums1[i])){
-                auxiliary[k] = nums2[j++];
-            } else {
-                auxiliary[k] = nums1[i++];
-            }
-        }
-        //move result from auxiliary array to unused (visited parts of source arrays)
-        int freezeI = i;
-        int freezeJ = j;
-        for (int x = 0; x < freezeI; x++){
-            nums1[x] = auxiliary[x];
-        }
-        for (int y = 0; y < freezeJ; y++){
-            nums2[y] = auxiliary[freezeI + y];
-        }
-        //finish first part, using auxiliary again
-        for (int k = 0; k < n; k++){
-            if (j < n && (i == n || nums2[j] <= nums1[i])){
-                auxiliary[k] = nums2[j++];
-            } else {
-                auxiliary[k] = nums1[i++];
-            }
-        }
+        int mid = a.length / 2;
+        int[] auxiliary = new int[mid];
 
-        int [] result = new int[2*n];
-        System.arraycopy(nums1, 0, result, 0, freezeI);
-        System.arraycopy(nums2, 0, result, freezeI, freezeJ);
-        System.arraycopy(auxiliary, 0, result, n, n);
-        return result;
+        System.arraycopy(a, 0, auxiliary, 0, mid);
+
+        int i = 0;
+        int j = mid;
+        for (int k = 0; k < a.length; k++){
+            if (j == a.length || (i < mid && auxiliary[i] < a[j])){
+                a[k] = auxiliary[i++];
+            } else {
+                a[k] = a[j++];
+            }
+        }
+        return a;
     }
 }
--- src/test/java/org/sedgewick/algorithms/part_one/week_three/question_one/MergeSortedArraysWithLimitTest.java
+++ src/test/java/org/sedgewick/algorithms/part_one/week_three/question_one/MergeSortedArraysWithLimitTest.java
@@ -9,13 +9,13 @@ class MergeSortedArraysWithLimitTest {
 
     @Test
     void merge() {
-        int[] result = new MergeSortedArraysWithLimit().merge(new int[] {1,3,5,7,9}, new int[] {0,2,4,6,8}, 5);
+        int[] result = new MergeSortedArraysWithLimit().merge(new int[] {1,3,5,7,9, 0,2,4,6,8});
         assertArrayEquals(new int[] {0,1,2,3,4,5,6,7,8,9}, result);
     }
 
     @Test
     void mergeTinyArrays() {
-        int[] result = new MergeSortedArraysWithLimit().merge(new int[] {0}, new int[] {1}, 1);
+        int[] result = new MergeSortedArraysWithLimit().merge(new int[] {1, 0});
         assertArrayEquals(new int[] {0,1}, result);
     }
 }
