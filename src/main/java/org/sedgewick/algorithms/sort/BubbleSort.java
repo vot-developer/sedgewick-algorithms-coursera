cf186c37cd290a96d3a5f89836d4f990e8585a9b
igor gorchakov <gorchakov.igiv@gmail.com>
2020-11-17 16:26:23 +0300

    added bubble sort

---
+++ src/main/java/org/sedgewick/algorithms/sort/BubbleSort.java
@@ -0,0 +1,22 @@
+package org.sedgewick.algorithms.sort;
+
+/**
+ * Bubble Sort algorithm
+ * <p>
+ * time - O(n*n)
+ * space - O(1)
+ */
+public class BubbleSort {
+
+    public void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length; j++) {
+                if (array[j - 1] > array[j]){
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+    }
+}
