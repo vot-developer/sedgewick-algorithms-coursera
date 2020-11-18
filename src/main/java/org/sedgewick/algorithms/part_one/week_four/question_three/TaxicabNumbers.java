121ae9ebd283df8602cd63267f5385da40a49da1
igor gorchakov <gorchakov.igiv@gmail.com>
2020-11-18 14:32:34 +0300

    added solving of 'Taxicab numbers' with O(n) storage

--- src/main/java/org/sedgewick/algorithms/part_one/week_four/question_three/TaxicabNumbers.java
+++ src/main/java/org/sedgewick/algorithms/part_one/week_four/question_three/TaxicabNumbers.java
@@ -1,15 +1,14 @@
 package org.sedgewick.algorithms.part_one.week_four.question_three;
 
-import edu.princeton.cs.algs4.MaxPQ;
+import edu.princeton.cs.algs4.MinPQ;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class TaxicabNumbers {
 
-    class Candidates implements Comparable<Candidates>{
+    class Candidates implements Comparable<Candidates> {
         final int a;
         final int b;
         final int sum;
@@ -28,32 +27,23 @@ public class TaxicabNumbers {
         }
     }
 
-    public List<List<Integer>> find(int max){
+    public List<List<Integer>> find(int max) {
         List<List<Integer>> result = new ArrayList<>();
         if (max <= 1) return result;
 
-        MaxPQ<Candidates> maxPQ = new MaxPQ<>();
+        MinPQ<Candidates> minPQ = new MinPQ<>();
 
         for (int i = 1; i < max; i++)
-            for (int j = i + 1; j < max; j++)
-                maxPQ.insert(new Candidates(i, j));
+            minPQ.insert(new Candidates(i, i));
 
-        List<Candidates> lr = new ArrayList<>();
-        while(maxPQ.size() > 1){
-            Candidates c = maxPQ.delMax();
-            if (c.sum == maxPQ.max().sum){
-                lr.add(c);
-                while(c.sum == maxPQ.max().sum){
-                    c = maxPQ.delMax();
-                    lr.add(c);
-                }
-                for (int i = 0; i < lr.size(); i++)
-                    for (int j = i + 1; j < lr.size(); j++)
-                        result.add(Arrays.asList(lr.get(i).a, lr.get(i).b, lr.get(j).a, lr.get(j).b));
-            }
-            lr.clear();
+        while (minPQ.size() > 1) {
+            Candidates c = minPQ.delMin();
+            if (c.sum == minPQ.min().sum)
+                result.add(Arrays.asList(c.a, c.b, minPQ.min().a, minPQ.min().b));
+
+            if (c.b < max)
+                minPQ.insert(new Candidates(c.a, c.b + 1)); // try to fit in O(n), use only n size and increase second value for all combinations
         }
-
         return result;
     }
 
--- src/test/java/org/sedgewick/algorithms/part_one/week_four/question_three/TaxicabNumbersTest.java
+++ src/test/java/org/sedgewick/algorithms/part_one/week_four/question_three/TaxicabNumbersTest.java
@@ -13,13 +13,13 @@ class TaxicabNumbersTest {
     void test() {
         List<List<Integer>> result = new TaxicabNumbers().find(13);
         assertEquals(1, result.size());
-        assertEquals(Arrays.asList(1, 12, 9, 10), result.get(0));
+        assertEquals(Arrays.asList(9, 10, 1, 12), result.get(0));
     }
 
     @Test
     void test2() {
         List<List<Integer>> result = new TaxicabNumbers().find(17);
         assertEquals(2, result.size());
-        assertEquals(Arrays.asList(2, 16, 9, 15), result.get(0));
+        assertEquals(Arrays.asList(9, 15, 2, 16), result.get(1));
     }
 }
