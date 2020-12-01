20fc9f6d42dae49244e330ed72a5b23742bc393c
igor gorchakov <gorchakov.igiv@gmail.com>
2020-12-01 12:19:06 +0300

    fix

--- src/main/java/org/sedgewick/algorithms/sort/BubbleSort.java
+++ src/main/java/org/sedgewick/algorithms/sort/BubbleSort.java
@@ -10,7 +10,7 @@ public class BubbleSort {
 
     public void sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 1; j < array.length; j++) {
+            for (int j = 1; j < array.length - i; j++) {
                 if (array[j - 1] > array[j]) {
                     int temp = array[j];
                     array[j] = array[j - 1];
