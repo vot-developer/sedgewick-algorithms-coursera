ecba3b3201629dba710f7d24010002177a1ede2c
igor gorchakov <gorchakov.igiv@gmail.com>
2020-12-05 15:59:59 +0300

    connection components

---
+++ src/main/java/org/sedgewick/algorithms/search/ConnectionComponent.java
@@ -0,0 +1,46 @@
+package org.sedgewick.algorithms.search;
+
+import org.sedgewick.algorithms.structures.Graph;
+
+public class ConnectionComponent {
+    private final Graph graph;
+    private final Integer[] id;
+    private final boolean[] marked;
+    private int idCount;
+
+    public ConnectionComponent(Graph graph) {
+        this.graph = graph;
+        this.id = new Integer[graph.V()];
+        this.marked = new boolean[graph.V()];
+
+        idCount = 0;
+        for (int i = 0; i < marked.length; i++) {
+            if (!marked[i]) {
+                dfs(i);
+                idCount++;
+            }
+        }
+    }
+
+    private void dfs(int v) {
+        marked[v] = true;
+        this.id[v] = idCount;
+
+        for (int i : graph.adj(v)) {
+            if (!marked[i])
+                dfs(i);
+        }
+    }
+
+    public boolean isConnected(int a, int b) {
+        return id[a] == id[b];
+    }
+
+    public int count() {
+        return idCount;
+    }
+
+    public int id(int v) {
+        return id[v];
+    }
+}
---
+++ src/test/java/org/sedgewick/algorithms/search/ConnectionComponentTest.java
@@ -0,0 +1,57 @@
+package org.sedgewick.algorithms.search;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.sedgewick.algorithms.structures.Graph;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectionComponentTest {
+    private static Graph graph;
+    /*
+    6 - 0 - 1 - 4 - 5 - 3
+       /
+      2
+
+      9 - 10    7-8
+     / \
+    11-12
+     */
+    @BeforeAll
+    static void setUp(){
+        graph = new Graph(13);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 6);
+        graph.addEdge(1, 4);
+        graph.addEdge(4, 5);
+        graph.addEdge(3, 5);
+        graph.addEdge(7, 8);
+        graph.addEdge(9, 10);
+        graph.addEdge(9, 11);
+        graph.addEdge(9, 12);
+        graph.addEdge(11, 12);
+    }
+
+    @Test
+    void isConnected() {
+        ConnectionComponent cc = new ConnectionComponent(graph);
+        assertTrue(cc.isConnected(3, 6));
+        assertTrue(cc.isConnected(10, 11));
+        assertTrue(cc.isConnected(7, 8));
+        assertFalse(cc.isConnected(0, 7));
+        assertFalse(cc.isConnected(9, 3));
+    }
+
+    @Test
+    void count() {
+        ConnectionComponent cc = new ConnectionComponent(graph);
+        assertEquals(3, cc.count());
+    }
+
+    @Test
+    void id() {
+        ConnectionComponent cc = new ConnectionComponent(graph);
+        assertEquals(1, cc.id(7));
+    }
+}
