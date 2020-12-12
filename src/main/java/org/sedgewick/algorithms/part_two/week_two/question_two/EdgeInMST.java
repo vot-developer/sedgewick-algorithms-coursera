9d709373759c41e2aac82afc121715838843167f
igor gorchakov <gorchakov.igiv@gmail.com>
2020-12-12 19:13:02 +0300

    fix solution

--- src/main/java/org/sedgewick/algorithms/part_two/week_two/question_two/EdgeInMST.java
+++ src/main/java/org/sedgewick/algorithms/part_two/week_two/question_two/EdgeInMST.java
@@ -3,16 +3,32 @@ package org.sedgewick.algorithms.part_two.week_two.question_two;
 import edu.princeton.cs.algs4.Edge;
 import edu.princeton.cs.algs4.EdgeWeightedGraph;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class EdgeInMST {
     public boolean isIn(Edge e, EdgeWeightedGraph g) {
         int v = e.either();
-        int w = e.other(v);
-        return isMin(e, v, g) || isMin(e, w, g);
+        return isConnectedByLessWeight(v, e.other(v), e.weight(), g);
     }
 
-    private boolean isMin(Edge e, int v, EdgeWeightedGraph g) {
-        for (Edge i : g.adj(v))
-            if (i.weight() < e.weight()) return false;
+    public boolean isConnectedByLessWeight(int v, int w, double weight, EdgeWeightedGraph g){
+        boolean[] marked = new boolean[g.V()];
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.addFirst(v);
+        marked[v] = true;
+        while (!stack.isEmpty()){
+            int vertex = stack.removeFirst();
+            if (vertex == w) return false;
+            for (Edge e : g.adj(vertex)){
+                if (e.weight() >= weight) continue;
+                int i = e.other(vertex);
+                if (!marked[i]) {
+                    stack.addFirst(i);
+                    marked[i] = true;
+                }
+            }
+        }
         return true;
     }
 }
