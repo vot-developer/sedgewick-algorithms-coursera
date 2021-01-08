7efd785efeeaccdf5480051085c339c1455b98b3
igor gorchakov <gorchakov.igiv@gmail.com>
2021-01-08 18:23:22 +0300

    solution of 'Challenging REs'

--- src/main/java/org/sedgewick/algorithms/part_two/week_five/question_one/ChallengingREs.java
+++ src/main/java/org/sedgewick/algorithms/part_two/week_five/question_one/ChallengingREs.java
@@ -4,7 +4,7 @@ public class ChallengingREs {
     //All strings except 11 or 111.
     public static final String REGEXP_1 = "0.*|1|10.*|110.*|111.+";
     //Strings with 1 in every odd-number bit position.
-    public static final String REGEXP_2 = "1|(1(.{1}1)*.?)";
+    public static final String REGEXP_2 = "1|(1(.1)*.?)";
     //Strings with at least two 0s and at most one 1.
     public static final String REGEXP_3 = "0*|100+|0+10+|00+10*";
     //Strings that when interpreted as a binary integer are a multiple of 3. (Hint - build DFA for binary string)
